bfb0cdb868cf82872e6e7f73687d17af3e82e47d
Thomas Wiringa <thomas.wiringa@gmail.com>
2017-03-28 20:55:38 +0200

    Add migration

---
+++ src/Migrations/2017_03_28_130100_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('username', 20)->unique();
+            $table->string('password');
+            $table->string('email', 100);
+            $table->boolean('active');
+            $table->boolean('manager')->default(false);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
